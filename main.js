a04ebfee62b25249a1b1bf7f1265d9397eddf4b1
Seth Yap <seth@pc-204-152.customer.ask4.lan>
2023-03-02 12:23:03 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger'); 
+const link = document.querySelector('.nav-link a');  
+ 
+hamburger.addEventListener('click', function(){
+   this.classList.toggle('is-active');
+  
+})
+
+
+
+
+
+
+
